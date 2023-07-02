717473f49dc7d4e12ea6c1786d77888c570194a2
Mikhail <mikhail.biazverkhi@gmail.com>
2023-07-02 00:32:21 -0400

    Correction seulRepas.php and css

--- seulRepas.php
+++ seulRepas.php
@@ -4,8 +4,8 @@ session_start();
 
 require './helpers/functions.php';
 
-if(!isset($_SESSION['user'])){
-  header('Location: /connexion.php');
+if (!isset($_SESSION['user'])) {
+    header('Location: /connexion.php');
 }
 
 $filename = __DIR__ . '/public/data/users.json';
@@ -19,23 +19,22 @@ $repasId = $_GET['id'];
 
 $users = getTableauUsers($filename);
 
-    foreach ($users as $user) {
-        if (isset($user['repas'])) {
-            foreach ($user['repas'] as $repas) {
-                if ($repas['repasId'] == $repasId) {
-                    $repasChoisi = $repas ?? [];
-                    $userId = $user['userId'] ?? '';
-                    $userPseudo = $user['pseudo'];
-                    break;
-                }
+foreach ($users as $user) {
+    if (isset($user['repas'])) {
+        foreach ($user['repas'] as $repas) {
+            if ($repas['repasId'] == $repasId) {
+                $repasChoisi = $repas ?? [];
+                $userId = $user['userId'] ?? '';
+                $userPseudo = $user['pseudo'];
+                break;
             }
         }
     }
+}
 
-    $indexUser = array_search($userId, array_column($users, 'userId'));
-    $indexRepas = array_search($repasId, array_column($users[$indexUser]['repas'], 'repasId'));
-    $repasAvis = $users[$indexUser]['repas'][$indexRepas]['avis'] ?? [];
-
+$indexUser = array_search($userId, array_column($users, 'userId'));
+$indexRepas = array_search($repasId, array_column($users[$indexUser]['repas'], 'repasId'));
+$repasAvis = $users[$indexUser]['repas'][$indexRepas]['avis'] ?? [];
 
 if (isset($_POST["envoyer"])) {
     $_POST = filter_input_array(INPUT_POST, [
@@ -63,13 +62,12 @@ if (isset($_POST["envoyer"])) {
             [...$users[$indexUser]['repas'][$indexRepas]['avis'] ?? [], [
             'note' => $note,
             'commentaire' => $commentaire,
-            'userPseudo' => $_SESSION['user']['pseudo']
+            'userPseudo' => $_SESSION['user']['pseudo'],
         ],
         ];
 
-
         writeTableauUsersInFile($filename, $users);
-        header('Location: /seulRepas.php?id='.$repasId);
+        header('Location: /seulRepas.php?id=' . $repasId);
     }
 
 }
@@ -87,105 +85,110 @@ if (isset($_POST["annuler"])) {
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>User account</title>
- <link rel="stylesheet" href="public/css/account.css">
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <?php require './includes/head.php'?>
+
+    <title>User account</title>
+    <link rel="stylesheet" href="public/css/seulRepas.css">
 </head>
 
 <body>
- <div class="container">
-  <div class="boxesContainer">
-   <div class="boxContent">
-    <div class="boxImgDiv">
-     <img src="<?=$repasChoisi['cheminImage']?>" class="img-fluid" alt="Food Image">
+
+<div class="container d-grid gap-3 px-4">
+    <div class="row">
+        <div class="col my-5">
+            <div class="text-center">
+                <?php if ($_SESSION['user']['userId'] !== $userId): ?>
+                    <h3>Le repas d'utilisateur: <span><?=$userPseudo?></span></h3>
+                <?php else: ?>
+                    <h3>Le repas d'utilisateur: <span><?=$_SESSION['user']['pseudo']?></span></h3>
+                <?php endif;?>
+            </div>
+        </div>
     </div>
 
-    <span class="imageTitle">
-        <?=$repasChoisi['nomRepas']?>
-    </span>
-    <span class="boxPrice">
-        <?=$repasChoisi['prixRepas']?>$
-    </span>
-    <span class="boxLocalisation">
-        <?=$repasChoisi['localisation']?>
-    </span>
+    <div class="row">
 
-    <!-- <div class="boxImgBtn">
-     <div><button type="button" class="modifier btn-primary">Modifier</button></div>
+        <div class="col d-flex align-content-center justify-content-center">
+            <img src="<?=$repasChoisi['cheminImage']?>" class="img-fluid" alt="Food Image">
+        </div>
 
-     <div><button type="button" class="supprimer btn-secondary">Supprimer</button></div>
-    </div> -->
-
-    <div class="imgDesc">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos excepturi aperiam sed voluptatem. Quos animi ullam quo aliquam, qui, at, reprehenderit quam sint explicabo doloribus ipsa! Eius, veritatis aliquam?
-    </div>
-    </div>
-
+        <div class="col d-grid gap-3">
+            <div>
+                <h4>Nom du repas: <?=$repasChoisi['nomRepas']?></h4>
+            </div>
+            <div>
+                <h4>Nom du repas: <?=$repasChoisi['prixRepas']?>$</h4>
+            </div>
+            <div>
+                <h4>Localisation: <?=$repasChoisi['localisation']?></h4>
+            </div>
+            <div>
+                <h4>Description: </h4>
+                <p><?=$repasChoisi['description']?></p>
+            </div>
+        </div>
   </div>
-  <div class="boxesContainer">
-  <!-- <div class="boxContent"> -->
 
-  <?php if($_SESSION['user']['userId'] !== $userId) : ?>
-    <!-- </div> -->
-    <h3>Le repas d'utilisateur: <span><?=$userPseudo?></span></h3>
-<br><br>
-    <h3>Entrez votre commentaire et votre note: </h3>
-    <form action="seulRepas.php?id=<?=$repasId?>" method="POST">
-        <!-- <input type="number" name="note" min=0 max=10/> -->
-        <label for="note">Votre note: </label>
-        <input type="text" name="note" id="note" value="<?=$note ?? ''?>">
-        <?php if ($errors['note']): ?>
-          <p style="color:red;"><?=$errors['note']?></p>
+  <div class="row">
+
+    <div class="col my-5">
+        <h3 class="text-center">Tous les commentaires: </h3>
+        <div style="overflow:scroll; height:300px;">
+            <table>
+                <tr>
+                  <th>Utilisateur</th>
+                  <th>Commentaire</th>
+                  <th>Note</th>
+                </tr>
+                <?php foreach ($repasAvis as $avis): ?>
+                <tr>
+                  <td><?=$avis['userPseudo']?></td>
+                  <td><?=$avis['commentaire']?></td>
+                  <td><?=$avis['note']?></td>
+                </tr>
+                <?php endforeach;?>
+            </table>
+        </div>
+    </div>
+
+    <div class="col my-5">
+
+        <?php if ($_SESSION['user']['userId'] !== $userId): ?>
+            <h3 class="text-center">Entrez votre commentaire et votre note: </h3>
+            <form action="seulRepas.php?id=<?=$repasId?>" method="POST">
+
+                <div class="mb-3">
+                    <label for="note" class="form-label">Votre note: </label>
+                    <input type="text" class="form-control" name="note" id="note" value="<?=$note ?? ''?>"/>
+                    <?php if ($errors['note']): ?>
+                        <p style="color:red;"><?=$errors['note']?></p>
+                    <?php endif;?>
+                </div>
+
+                <div class="mb-3">
+                    <label for="commentaire" class="form-label">Votre commentaire: </label>
+                    <textarea class="form-control" name="commentaire" id="commentaire" rows="3"></textarea>
+                    <?php if ($errors['commentaire']): ?>
+                        <p style="color:red;"><?=$errors['commentaire']?></p>
+                    <?php endif;?>
+                </div>
+                <div class="d-flex justify-content-end gap-5" >
+                    <input class="btn btn-primary" type="submit" name="envoyer" value="Envoyer">
+                    <input class="btn btn-secondary" type="submit" name="annuler" value="Annuler">
+                </div>
+            </form>
+
+        <?php else :?>
+            <div class="d-flex justify-content-around" >
+                <a class="btn btn-primary" role="button" href="#">Modifier</a>
+                <a class="btn btn-secondary" role="button" href="/index.php">Annuler</a>
+                <a class="btn btn-danger" role="button" href="#">Supprimer</a>
+            </div>
         <?php endif;?>
-
-<br><br>
-
-        <label for="commentaire">Votre commentaire: </label>
-        <input name="commentaire" id="commentaire" value="<?=$commentaire ?? ''?>">
-        <!-- <textarea name="commentaire" id="commentaire" cols="50" rows="3">
-        </textarea> -->
-        <?php if ($errors['commentaire']): ?>
-          <p style="color:red;"><?=$errors['commentaire']?></p>
-        <?php endif;?>
-
-        <input type="submit" name="envoyer" value="Envoyer">
-        <input type="submit" name="annuler" value="Annuler">
-    </form>
-    <?php else :?>
-        <h3>Le repas d'utilisateur: <span><?=$_SESSION['user']['pseudo']?></span></h3>
-    <?php endif;?>
-<br><br>
-
-    <h3>Tous les commentaires: </h3>
-    <table>
-  <tr>
-    <th>Utilisateur</th>
-    <th>Commentaire</th>
-    <th>Note</th>
-  </tr>
-  <?php foreach ($repasAvis as $avis): ?>
-  <tr>
-    <td><?=$avis['userPseudo']?></td>
-    <td><?=$avis['commentaire']?></td>
-    <td><?=$avis['note']?></td>
-  </tr>
-  <?php endforeach;?>
-    </table>
-
-<br><br>
-    <?php if($_SESSION['user']['userId'] == $userId) : ?>
-
-        <button>Modifier</button>
-
-<br><br>
-        <button>Supprimer</button>
-    <?php endif;?>
+    </div>
+  </div>
 </div>
 
-</div>
 </body>
 
 </html>
